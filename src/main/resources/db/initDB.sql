074d015f9260ff85171a66403c857a23f6db54da
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2018-10-24 21:30:50 +0300

    HW03: bug fix, sql:init&populate DB meals, add jdbcMealRepository

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
@@ -23,3 +24,12 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  dateTime    TIMESTAMP,
+  description VARCHAR,
+  calories INTEGER,
+  user_id INTEGER
+ );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,20 @@
 DELETE FROM user_roles;
 DELETE FROM users;
-ALTER SEQUENCE global_seq RESTART WITH 100000;
+DELETE FROM meals;
+ALTER SEQUENCE global_seq  RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES ('2018-10-24 10:00:00', 'Завтрак', 140, 100000),
+       ('2018-10-24 14:00:00', 'Обед', 500, 100000),
+       ('2018-10-24 19:00:00', 'Ужин', 700, 100000),
+       ('2018-10-25 10:00:00', 'Завтрак', 100, 100000),
+       ('2018-10-25 14:00:00', 'Обед', 500, 100000),
+       ('2018-10-25 19:00:00', 'Ужин', 502, 100000);
